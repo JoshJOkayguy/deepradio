37b061491afe5906875915f71120582da14c8b00
JoshJOkayguy <55960406+JoshJOkayguy@users.noreply.github.com>
2021-04-29 21:46:08 -0400

    ok finally

--- lua/deepradio/cl_init.lua
+++ lua/deepradio/cl_init.lua
@@ -4,13 +4,7 @@ net.Receive("DeepRadio:SendMessage", function()
     local sender = net.ReadString()
     local msg = net.ReadString()
 
-    local capchnl = string.Explode("", chnl)
-
-    capchnl[1] = string.upper(capchnl[1])
-
-    capchnl = table.concat(capchnl, "")
-
-    chat.AddText(color_white, "[",DeepRadio.ChannelColors[chnl], capchnl, color_white, "] "..sender..": "..msg)
+    chat.AddText(color_white, "[",DeepRadio.ChannelColors[chnl], prfx, color_white, "] "..sender..": "..msg)
 end )
 
 MsgC("\n[Deep Radio]",DeepRadio.Success, " Clientside initialized.")
